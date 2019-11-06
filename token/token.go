3475dd5cdb47fbc7b32e044c707be4db22816993
istsh <github3104xniiapm@gmail.com>
2019-11-07 01:20:17 +0900

    Refactoring: token

--- token/token.go
+++ token/token.go
@@ -2,17 +2,13 @@ package token
 
 type TokenType string
 
-// 各文字列が何を意味しているのかを対応づける為に、定数を設けている。
 const (
 	NONE = "NONE"
 
-	// ILLEGAL = "ILLEGAL" // 解析に失敗した場合に設定するTokenType
-	EOF = "EOF" // コードの終了
+	EOF = "EOF"
 
-	//INT    = "INT"
-	STRING = "STRING" // 文字列
+	STRING = "STRING"
 
-	// 識別子
 	HEADING1 = "HEADING1"
 	HEADING2 = "HEADING2"
 	HEADING3 = "HEADING3"
@@ -54,72 +50,20 @@ const (
 	LINK_BEGIN       = "LINK_BEGIN"
 	LINK_FINISH      = "LINK_FINISH"
 
-	//
-
 	TAB1 = "TAB1"
 	TAB2 = "TAB2"
 	TAB3 = "TAB3"
 
 	SPACE = "SPACE"
-	//SPACE4 = "    "
 
 	LINE_FEED_CODE = "LINE_FEED_CODE"
 
 	HYPHEN = "HYPHEN"
-
-	//ASTERISK_ITALIC         = "*"
-	//ASTERISK_BOLD           = "**"
-	//ASTERISK_ITALIC_BOLD    = "***"
-	//UNDER_SCORE_ITALIC      = "_"
-	//UNDER_SCORE_BOLD        = "__"
-	//UNDER_SCORE_ITALIC_BOLD = "___"
-
-	//DOT         = "."
-	//ASTERISK    = "*"
-	//UNDER_SCORE = "_"
-	//
-	//PLUS = "+"
-	//
-	//LPAREN   = "LPAREN"
-	//RPAREN   = "RPAREN"
-	//LBRACKET = "LBRACKET"
-	//RBRACKET = "RBRACKET"
-	//
-	//// ここから下は不要かも
-	//ASSIGN = "="
-	//
-	//BANG = "!"
-	//
-	//SLASH = "/"
-	//
-	//GT = ">"
-	//LT = "<"
-	//
-	//EQ     = "=="
-	//NOT_EQ = "!="
-	//
-	//// デリミタ
-	//COMMA     = ","
-	//SEMICOLON = ";"
-	//COLON     = ":"
-	//
-	//LBRACE = "{"
-	//RBRACE = "}"
-	//
-	//// キーワード
-	//FUNCTION = "FUNCTION"
-	//LET      = "LET"
-	//TRUE     = "TRUE"
-	//FALSE    = "FALSE"
-	//IF       = "IF"
-	//ELSE     = "ELSE"
-	//RETURN   = "RETURN"
-	//MACRO    = "MACRO"
 )
 
 type Token struct {
-	Type    TokenType // トークンタイプ
-	Literal []byte    // トークンリテラル、Typeに応じたbyte配列
+	Type    TokenType
+	Literal []byte
 }
 
 func GetHeadingToken(cnt int) TokenType {
@@ -137,7 +81,6 @@ func GetHeadingToken(cnt int) TokenType {
 	case 6:
 		return HEADING6
 	default:
-		//return ILLEGAL
 		return STRING
 	}
 }
@@ -151,7 +94,6 @@ func GetTabToken(cnt int) TokenType {
 	case 3:
 		return TAB3
 	default:
-		//return ILLEGAL
 		return STRING
 	}
 }
@@ -165,7 +107,6 @@ func GetAsteriskToken(cnt int) TokenType {
 	case 3:
 		return ASTERISK_ITALIC_BOLD_BEGIN
 	default:
-		//return ILLEGAL
 		return STRING
 	}
 }
@@ -179,7 +120,6 @@ func GetUnderScoreToken(cnt int) TokenType {
 	case 3:
 		return UNDER_SCORE_ITALIC_BOLD_BEGIN
 	default:
-		//return ILLEGAL
 		return STRING
 	}
 }
@@ -191,7 +131,6 @@ func GetCitationToken(cnt int) TokenType {
 	case 2:
 		return CITATION2
 	default:
-		//return ILLEGAL
 		return STRING
 	}
 }
